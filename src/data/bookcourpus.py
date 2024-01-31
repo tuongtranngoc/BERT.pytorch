dd13a9edd4ef60bd1c5785370a8f5b4579da06d4
tuongtranngoc <ngoctuongwindy@gmail.com>
2024-01-31 21:31:08 +0700

    update: dataset

--- src/data/bookcourpus.py
+++ src/data/bookcourpus.py
@@ -16,21 +16,19 @@ class BookCorpusDataset(nn.Module):
     def __init__(self) -> None:
         super(BookCorpusDataset, self).__init__()
         self.dataset = datasets.load_dataset(cfg['Train']['bookcorpus'])
-        self.batch_size = cfg['Train']['batch_size']
-        self.word_dict = {
-            '[PAD]': 0,
-            '[CLS]': 1,
-            '[SEP]': 2,
-            '[MASK]': 3
-        }
+
     def _get_next_sentence(self, sentence, next_sentence, paragraphs):
         if random.random() < 0.5:
-            is_next=True
+            is_next =True
         else:
             next_sentence = random.choice(random.choices(paragraphs))
         return sentence, next_sentence, is_next
     
     def _get_nsp_data_from_paragraph(self, pragraph, pragraphs, vocab, max_len):
+        """Generating the Next Sentence Prediction
+            max_len: maximum length of a BERT input sequence during pretraining
+            pragraph: A list of sentences, where each sentence is a list of tokens
+        """
         nps_data_from_paragraph = []
         for i in range(len(pragraph)-1):
             tokens_a, tokens_b, is_next = self._get_next_sentence(pragraph[i], pragraph[i+1], pragraphs)
@@ -50,6 +48,11 @@ class BookCorpusDataset(nn.Module):
         return tokens, segments
     
     def _replace_maskedlm_tokens(self, tokens, cand_pred_positions, num_mlm_preds, vocab):
+        """Replace mask for tokens
+            tokens: A list of tokens representing a BERT input sentence
+            cand_pred_positions: A list of token indices of BERT input sequence
+            num_mlm_preds: the number of predictions            
+        """
         mlm_input_tokens = [token for token in tokens]
         pred_position_and_labels = []
         random.shuffle(cand_pred_positions)
@@ -66,8 +69,7 @@ class BookCorpusDataset(nn.Module):
                 else:
                     masked_token = random.choice(vocab.idx_to_token)
             mlm_input_tokens[mlm_pred_position] = masked_token
-            pred_position_and_labels.append(
-                (mlm_pred_position, tokens[mlm_pred_position]))
+            pred_position_and_labels.append((mlm_pred_position, tokens[mlm_pred_position]))
             
         return mlm_input_tokens, pred_position_and_labels
     
@@ -88,5 +90,25 @@ class BookCorpusDataset(nn.Module):
         return vocab[mlm_input_tokens], pred_positions, vocab[mlm_pred_labels]
     
     def _pad_bert_inputs(self, examples, max_len, vocab):
-        pass
+        """Append the specical <pad> tokens to inputs
+            examples: the outputs from the helper functions nsp and mlm
+        """
+        max_num_mlm_preds = round(max_len * 0.15)
+        all_token_ids, all_segments, valid_lens = [], [], []
+        all_pred_positions, all_mlm_weights, all_mlm_labels = [], [], []
+        nsp_labels = []
+
+        for (token_ids, pred_positions, mlm_pred_label_ids, segments, is_next) in examples:
+            all_token_ids.append(torch.tensor(token_ids + [vocab['<pad>']] * (max_len - len(token_ids)), dtype=torch.long))
+            all_segments.append(torch.tensor(segments + [0] * (max_len - len(segments)), dtype=torch.long))
+            valid_lens.append(torch.tensor(len(token_ids), dtype=torch.float32))
+            all_pred_positions.append(torch.tensor(pred_positions + [0] * (max_num_mlm_preds - len(pred_positions)), dtype=torch.long))
+
+            all_mlm_weights.append(torch.tensor([1.0] * len(mlm_pred_label_ids) + [0.0] * (max_num_mlm_preds - len(pred_positions)), dtype=torch.float32))
+            all_mlm_labels.append(torch.tensor(mlm_pred_label_ids + [0] * (max_num_mlm_preds - len(mlm_pred_label_ids)), dtype=torch.long))
+            nsp_labels.append(torch.tensor(is_next, dtype=torch.long))
+
+        return (all_token_ids, all_segments, valid_lens, all_pred_positions, all_mlm_weights, all_mlm_labels, nsp_labels)
+    
+    
 

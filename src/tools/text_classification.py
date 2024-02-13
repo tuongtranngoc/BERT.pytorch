315134705b6cef44251ef8469daeacbc84e11a75
tuongtranngoc <ngoctuongwindy@gmail.com>
2024-02-13 16:53:40 +0700

    update: single text cls

--- src/tools/text_classification.py
+++ src/tools/text_classification.py
@@ -2,7 +2,33 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+import os
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
 
-class Predictor:
-    def __init__(self) -> None:
+from .. import config
+from src.utils.logger import Logger
+from src.utils.metrics import BatchMeter
+from src.utils.data_utils import DataUtils
+from src.utils.tensorboard import Tensorboard
+from src.models.pretrain.bert import BERTModel
+
+logger = Logger.get_logger('TRAINING')
+
+
+class Trainer:
+    def __init__() -> None:
         pass
+
+    def load_pretrained_model(self):
+        self.pretrained_model = BERTModel(vocab_size=len(self.dataset.vocab), 
+                               num_hiddens=config['num_hiddens'],
+                               ffn_num_hiddens=config['ffn_num_hiddens'],
+                               num_heads=config['num_heads'],
+                               num_blks=config['num_blks'],
+                               dropout=config['dropout'],
+                               max_len=config['max_len']).to(config['device'])
+        self.pretrained_model.load_state_dict(config['pretrain_checkpoints']['best_ckpt_path'])
+        
+
